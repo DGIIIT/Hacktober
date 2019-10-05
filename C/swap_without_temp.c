83f42ccdd68979ec3be74321cb6094cdb972b65f
Saad Aslam <saadaslam816@gmail.com>
2019-10-05 20:23:34 +0500

    FIX:badge

--- C/swap_without_temp.c
+++ C/swap_without_temp.c
@@ -4,9 +4,9 @@ int main()
     int x = 10, y = 5;  //sample
   
      
-    x = x + y;
-    y = x - y; 
-    x = x - y; 
+    x = x * y;
+    y = x / y; 
+    x = x / y; 
   
     printf("After Swapping: x = %d, y = %d", x, y); 
   
