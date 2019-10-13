005947e9f041dc214b54928749d6104c0ae58005
Michelle Cordeiro <42891377+MichelleCordeiro@users.noreply.github.com>
2019-10-13 16:30:02 -0300

    Add swaping one line and swaping without XOR C++

---
+++ Swaping one line/Swaping_1_Line_Multiplication.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+
+using namespace std;
+
+int main() {
+
+    int x = 5, y = 7;
+    cout<<"Before swapping: x = "<< x <<" and y = "<< y << endl;
+
+    x = (x * y) / (y = x);
+    cout<<"After swapping: x = "<< x <<" and y = "<< y << endl;
+
+    return 0;
+}
