d074e51cf11aa21d22d276140ef93b38b9bc563d
Arju Aman <33257286+arjuaman@users.noreply.github.com>
2019-10-23 00:54:10 +0530

    Create printing.c

---
+++ C/printing.c
@@ -0,0 +1,9 @@
+#include <stdio.h> 
+int main(void) 
+{ 
+    // Here we are using \n, which 
+    // is a new line character. 
+    printf("Hello\n"); 
+    printf("GeeksforGeeks"); 
+    return (0); 
+} 
