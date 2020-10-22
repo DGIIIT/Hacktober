9f8ca0ff015cbb8bd39d41d1408ec74c9f4ef463
Théo GIMENEZ <45224847+TheoGimenez7@users.noreply.github.com>
2020-10-22 02:50:34 +0200

    Create FakeVirus.cmd
    This is a CMD file that fakes virus install, it does nothing offensive.
    You can easily close it using ALT F4 or by closing the window.
    At the end of the execution, it shutdown the computer.

---
+++ OS/FakeVirus.cmd
@@ -0,0 +1,150 @@
+***This is a CMD file that fakes virus install, it does nothing offensive.
+***You can easily close it using ALT F4 or by closing the window.
+***At the end of the execution, it shutdown the computer.
+
+
+
+@ECHO OFF
+mode 200
+color 47
+net stop themes >nul
+title DEEP VIRAL INFECTION!
+echo VIRAL INFECTION!!!
+echo VIRAL INFECTION!!!
+echo VIRAL INFECTION!!!
+echo ERROR!!!
+echo -
+echo virus - TROJAN_DEMOLISHER code #45643676
+echo -
+echo FIREWALL - FAILED
+echo -
+echo ANTI-VIRUS - FAILED
+echo -
+echo IP ADDRESS BREACHED!
+echo -
+echo VIRUS ATTAINING: ****-****-****-8894
+echo -
+
+ping 0.0.0.0 -n 2 > NUL
+cls
+echo -
+echo SCANNING INFECTED AREAS...
+echo -
+
+ping 0.0.0.0 -n 2 > NUL
+
+set /a num=0
+:repeat1
+set /a num=%num% +1
+echo %num%
+if %num%==100 goto end
+goto repeat1
+:end
+cls
+echo -
+echo 86.5 PERCENT OF MEMORY INFECTED
+echo -
+echo INFECTION FATAL!
+echo -
+echo DELETION OF ENTIRE CONTENTS OF LOCAL DISK C: REQUIRED
+echo - 
+
+ping 0.0.0.0 -n 2 > NUL
+cls
+echo -
+echo DELETING HARD-DRIVE C:
+echo -
+dir /s
+
+ping 0.0.0.0 -n 2 > NUL
+cls
+echo -
+echo CONTENTS OF HARD-DRIVE C: ERASED
+echo -
+
+ping 0.0.0.0 -n 2 > NUL
+cls
+echo -
+echo SCANNING...
+echo -
+
+set /a num1=0
+:repeat2
+set /a num1=%num1% +1
+echo %num1%
+if %num1%==100 goto end1
+goto repeat2
+:end1
+cls
+echo -
+echo 0.00 PERCENT OF HARD-DRIVE INFECTED
+echo - 
+ping 0.0.0.0 -n 2 > NUL
+
+echo ERROR
+echo ERROR
+echo ERROR
+echo ERROR
+echo ERROR
+echo ERROR
+echo ERROR
+echo ERROR
+echo ERROR
+echo ERROR
+echo ERROR
+echo ERROR
+echo ERROR
+echo ERROR
+echo ERROR
+echo ERROR
+echo ERROR
+echo ERROR
+echo ERROR
+
+ping 0.0.0.0 -n 2 > NUL
+cls
+title SYSTEM FAILURE
+color 17
+echo ERROR!
+echo -
+echo VISUAL MEMORY LOST!
+echo -
+echo RAM LOST!
+echo -
+echo CORE PROCESSOR FAILING...
+echo -
+echo TOTAL SYSTEM CRASH IMMINENT!
+echo -
+echo -
+
+ping 0.0.0.0 -n 3 > NUL
+cls
+echo -
+echo -
+echo -
+echo SHUTDOWN COMPUTER NOW TO AVOID RISK OF FIRE!
+echo -
+echo -
+echo -
+
+ping 0.0.0.0 -n 3 > NUL
+cls
+echo -
+echo -
+echo -
+echo SEEK PROFESSIONAL HELP IMMEDIATLY TO PREVENT FURTHER DAMAGE!
+echo -
+echo -
+echo -
+ping 0.0.0.0 -n 4 > NUL
+rundll32.exe powrprof.dll,SetSuspendState 0,1,0
+
+
+
+
+*** You can comment this line if you don't want the system to shutdown
+powercfg -h on -s -t 60 -c "SYSTEM FAILURE<SHUTTING DOWN TO AVOID FURTHER DAMAGE!"
+
+
+**NB: Type "shutdown -a" in your Run box to abort a system shutdown
+***Type "net start themes" to in your Run box to get the themes service working
